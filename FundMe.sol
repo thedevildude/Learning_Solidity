be221167d632721164dd57fe8bf90782e91361b8
Devdeep Ghosh <63492939+thedevildude@users.noreply.github.com>
2021-09-14 01:57:49 +0530

    Create FundMe.sol

---
+++ FundMe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+contract FundMe {
+    
+    mapping(address => uint256) public addressToAmountFunded;
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+}

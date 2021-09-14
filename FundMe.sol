19b6608c78ec2058f07f0fecc65bc02f8f6b223b
Devdeep Ghosh <63492939+thedevildude@users.noreply.github.com>
2021-09-15 00:02:23 +0530

    Learned ETH - USD Conversion functions

--- FundMe.sol
+++ FundMe.sol
@@ -2,10 +2,29 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     
     mapping(address => uint256) public addressToAmountFunded;
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
+    }
+    
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+    
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+    }
+    
+    function getConversionRates(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
     }
 }

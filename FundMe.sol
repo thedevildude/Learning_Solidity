906be6ca6f95780ce7301b2f896b0e9792875241
Devdeep Ghosh <63492939+thedevildude@users.noreply.github.com>
2021-09-15 01:34:30 +0530

    Limited Withdrawal Only to Owner

--- FundMe.sol
+++ FundMe.sol
@@ -3,12 +3,27 @@
 pragma solidity >=0.6.6 <0.9.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
     
+    using SafeMathChainlink for uint256;
+    
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
     function fund() public payable {
+        
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRates(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
     
     function getVersion() public view returns(uint256) {
@@ -27,4 +42,19 @@ contract FundMe {
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
     }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function withdraw() payable onlyOwner public {
+        
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    } 
 }
